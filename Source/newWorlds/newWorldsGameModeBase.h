242bb993567aee3b1588dd4b1800e594010935d8
Grzegorz Skwarek <grzegorz.skwarek.95@gmail.com>
2019-01-05 22:30:02 +0100

    Initial commit.

---
+++ Source/newWorlds/newWorldsGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "newWorldsGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NEWWORLDS_API AnewWorldsGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
